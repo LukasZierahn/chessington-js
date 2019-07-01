445e3f14398149b63b638d6323f2a10f5012f632
LukasZierahn <lukas@kappa-mm.de>
2019-07-01 10:43:08 +0100

    made pawns move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from './../player';
+import Square from './../square';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,24 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let moves = [];
+
+        const currentSquare = board.findPiece(this);
+        
+
+        let pushDir = 0
+        if (this.player == Player.WHITE) {
+            pushDir = 1;
+        } else {
+            pushDir = -1;
+        }
+
+        let targetSquare = new Square(currentSquare.row + pushDir, currentSquare.col);
+        if (board.getPiece(targetSquare) === undefined) {
+            moves.push(targetSquare);
+        }
+        // console.log(moves);
+
+        return moves;
     }
 }
